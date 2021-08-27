a762d3186defd53ff8261712c06e8b30486f38cc
taylor <taylor@teukka.tech>
2021-08-27 12:31:25 +0300

    set text width

--- init.lua
+++ init.lua
@@ -32,6 +32,8 @@ set.tabstop         = 2
 set.synmaxcol       = 200
 set.cmdheight       = 2
 set.updatetime      = 200
+-- auto break at 80 chars
+set.textwidth       = 80
 set.conceallevel    = 2
 set.cscopetagorder  = 0
 set.cscopepathcomp  = 3
