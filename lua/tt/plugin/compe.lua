9b8957912be119c49f24ac984a7166f5d1e97e87
taylor <taylor@teukka.tech>
2022-04-24 23:21:28 +0300

    Update cmp config

--- lua/tt/plugin/compe.lua
+++ lua/tt/plugin/compe.lua
@@ -52,8 +52,10 @@ cmp.setup({
 		{ name = "path" },
 	},
 
-	documentation = {
-		border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+	window = {
+		documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered(),
+		--{ "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
 		-- 	winhighlight = "FloatBorder",
 	},
 
@@ -67,7 +69,7 @@ cmp.setup({
 	},
 
 	experimental = {
-		native_menu = false,
+		-- native_menu = true,
 	},
 })
 local tabnine = require("cmp_tabnine.config")
