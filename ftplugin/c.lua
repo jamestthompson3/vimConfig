f9d09461549d427d0a70eb169834de3ea939991c
taylor thompson <taylor@teukka.tech>
2026-04-22 23:00:37 -0700

    C header snippet

--- ftplugin/c.lua
+++ ftplugin/c.lua
@@ -3,3 +3,16 @@ vim.opt_local.formatoptions:remove({ "r", "o" })
 
 require("tt.lsp.efm").init()
 vim.lsp.start(vim.lsp.config.efm)
+
+-- Auto-insert include guard for new files
+if vim.fn.line("$") == 1 and vim.fn.getline(1) == "" then
+  local guard = vim.fn.expand("%:t"):upper():gsub("[^%w]", "_")
+  vim.api.nvim_buf_set_lines(0, 0, 0, false, {
+    "#ifndef " .. guard,
+    "#define " .. guard,
+    "",
+    "",
+    "#endif /* " .. guard .. " */",
+  })
+  vim.api.nvim_win_set_cursor(0, { 4, 0 })
+end
