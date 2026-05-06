63ac73b9b5c65b3a2ec7cb2f361fe3b35921dc0c
teukka <taylor@teukka.tech>
2026-05-06 15:06:35 -0700

    prevent biome lsp from using .git fallback

--- lsp/biome.lua
+++ lsp/biome.lua
@@ -1,8 +1,15 @@
 local node = require("tt.nvim_utils").nodejs
+local biome_roots = require("tt.constants").biome_roots
 
 return {
 	cmd = { node.get_node_bin("biome"), "lsp-proxy" },
-	root_markers = { "biome.json", "biome.jsonc" },
+	root_markers = biome_roots,
+	root_dir = function(bufnr, on_dir)
+		local root = vim.fs.root(bufnr, biome_roots)
+		if root then
+			on_dir(root)
+		end
+	end,
 	filetypes = {
 		"astro",
 		"css",
